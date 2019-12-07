fc0cb921dd790a3f82d074cdc3f834c71262436f
GamingLiamStudios <swimmingliam@gmail.com>
2019-12-07 20:29:40 +1100

    Added VAOs & VBOs

---
+++ src/com/GLS/LWJGL2G/renderEngine/RawModel.java
@@ -0,0 +1,20 @@
+package com.GLS.LWJGL2G.renderEngine;
+
+public class RawModel {
+	
+	int vaoID, vertexCount;
+
+	public RawModel(int vaoID, int vertexCount) {
+		this.vaoID = vaoID;
+		this.vertexCount = vertexCount;
+	}
+
+	public int getVaoID() {
+		return vaoID;
+	}
+
+	public int getVertexCount() {
+		return vertexCount;
+	}
+
+}

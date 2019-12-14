865659cfca7d74b13b45c180612ab2c0dd531d9a
GamingLiamStudios <swimmingliam@gmail.com>
2019-12-14 14:17:43 +1100

    Added Terrain

--- src/com/GLS/LWJGL2G/engineTester/MainGameLoop.java
+++ src/com/GLS/LWJGL2G/engineTester/MainGameLoop.java
@@ -13,18 +13,24 @@ import com.GLS.LWJGL2G.renderEngine.DisplayManager;
 import com.GLS.LWJGL2G.renderEngine.Loader;
 import com.GLS.LWJGL2G.renderEngine.MasterRenderer;
 import com.GLS.LWJGL2G.renderEngine.OBJLoader;
+import com.GLS.LWJGL2G.terrains.Terrain;
 import com.GLS.LWJGL2G.textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		ArrayList<Entity> objects = new ArrayList<>();
+		ArrayList<Terrain> terrains = new ArrayList<>();
 		DisplayManager display = new DisplayManager(1280,720,"Java Game");
 		Loader loader = new Loader();
 		MasterRenderer renderer = new MasterRenderer();
-		objects.add(new Entity(new TexturedModel(OBJLoader.loadObjModel("dragon", loader),new ModelTexture(loader.loadTexture("dragonTexture")).setShineDamper(10).setReflectivity(1)),new Vector3f(0,0,-25f),0,0,0,1f));
+		objects.add(new Entity(new TexturedModel(OBJLoader.loadObjModel("stall", loader),new ModelTexture(loader.loadTexture("dragonTexture")).setShineDamper(10).setReflectivity(1)),new Vector3f(0,0,-25f),0,0,0,1f));
 		Camera camera = new Camera();
-		Light light = new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
+		Light light = new Light(new Vector3f(3000,2000,3000),new Vector3f(1,1,1));
+		terrains.add(new Terrain(-1,0,loader,new ModelTexture(loader.loadTexture("grass"))));
+        terrains.add(new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass"))));
+        terrains.add(new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("grass"))));
+        terrains.add(new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass"))));
 		long pastTime = System.nanoTime();
 		long delta = 1;
 		long delta2 = 1;
@@ -32,13 +38,13 @@ public class MainGameLoop {
 		long ns = 1000000000;
 		int fps = 1;
 		long cap = ns/120;
-		System.out.println(cap);
 		while(!Display.isCloseRequested()) {
 	        if(cap/delta2>=1) {
 	        	delta2-=cap;
 	        	objects.get(0).increaseRotation(0, 1, 0);
 				camera.move();
 	        }
+	        for(Terrain terrain:terrains) renderer.processTerrain(terrain);
 			for(Entity entity:objects) renderer.processEntity(entity);
 			renderer.render(light, camera);
 			display.updateDisplay();
--- src/com/GLS/LWJGL2G/renderEngine/EntityRenderer.java
+++ src/com/GLS/LWJGL2G/renderEngine/EntityRenderer.java
@@ -17,26 +17,12 @@ import com.GLS.LWJGL2G.shaders.StaticShader;
 import com.GLS.LWJGL2G.textures.ModelTexture;
 import com.GLS.LWJGL2G.toolbox.Maths;
 
-public class Renderer {
+public class EntityRenderer {
 	
-	private static final float FOV = 70;
-	private static final float NEAR_PLANE = 0.1f;
-	private static final float FAR_PLANE = 1000;
-	
-	private Matrix4f projectionMatrix;
 	private StaticShader shader;
 	
-	public void prepare() {
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glClearColor(1, 0, 0, 1);
-	}
-	
-	public Renderer(StaticShader shader) {
-		GL11.glEnable(GL11.GL_CULL_FACE);
-		GL11.glCullFace(GL11.GL_BACK);
+	public EntityRenderer(StaticShader shader, Matrix4f projectionMatrix) {
 		this.shader = shader;
-		createProjectionMatrix();
 		shader.start();
 		shader.loadProjectionMatrix(projectionMatrix);
 		shader.stop();
@@ -76,20 +62,6 @@ public class Renderer {
 	private void prepareInstance(Entity entity) {
 		Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(), entity.getRx(), entity.getRy(), entity.getRz(), entity.getS());
 		shader.loadTransformationMatrix(transformationMatrix);
-	}
-	
-	private void createProjectionMatrix() {
-		float aR = (float) Display.getWidth()/(float) Display.getHeight();
-		float ys = (float) ((1f/Math.tan(Math.toRadians(FOV/2f)))*aR);
-		float xs = ys/aR;
-		float fl = FAR_PLANE-NEAR_PLANE;
-		projectionMatrix = new Matrix4f();
-		projectionMatrix.m00 = xs;
-		projectionMatrix.m11 = ys;
-		projectionMatrix.m22 = -((FAR_PLANE+NEAR_PLANE)/fl);
-		projectionMatrix.m23 = -1;
-		projectionMatrix.m32 = -((2*NEAR_PLANE*FAR_PLANE)/fl);
-		projectionMatrix.m33 = 0;
 	}
 
 }
